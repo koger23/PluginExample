abca932ef16749326b78dfb5722021742ef4b418
Gergely Kocsis <koger23@gmail.com>
2022-12-13 00:40:07 +0100

    Add Action project and sample action

---
+++ Actions/TextToUpperCase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace kogerohu.Actions
+{
+    public class TextToUpperCase : IPlugin
+    {
+        /// <summary>
+        /// Action to convert an input text to upper case and return
+        /// </summary>
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext executionContext = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            var inputText = (string)executionContext.InputParameters["inputText"];
+
+            if (string.IsNullOrWhiteSpace(inputText)) return;
+
+            var outputText = inputText.ToUpper();
+
+            executionContext.OutputParameters["outputText"] = outputText;
+        }
+    }
+}
